0cc636be033526f8ae18419cab97c69943e0e383
qwertyniop1 <svitas1997@gmail.com>
2017-05-29 19:02:06 +0300

    Fix animation issue.

--- Platformer/source/AnimationManager.cpp
+++ Platformer/source/AnimationManager.cpp
@@ -99,8 +99,9 @@ bool AnimationManager::loadFromXML(std::string fileName, sf::Texture &texture)
             cut = cut->NextSiblingElement("cut");
         }
 
-        anim.sprite.setOrigin(0, anim.frames[0].height);
-        
+        anim.sprite.setOrigin(0, anim.frames[anim.frames.size()-1].height);
+        // anim.sprite.setOrigin(0, anim.frames[0].height);
+
         animationsList[currentAnimation] = anim;
         animElement = animElement->NextSiblingElement("animation");
     }
